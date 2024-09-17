f45cc0d69dd56a5341e1ef93a04f2414e6e50a4b
felix <felixmaundu1@gmail.com>
2024-09-17 09:19:42 +0300

    correct naming structure

--- src/app/company-name/page.tsx
+++ src/app/company-name/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import { useState } from "react"
 import Link from "next/link"
 
-export default function company_name () {
+export default function Company_name () {
     const [companyName, setCompanyName] = useState("");
 
     return (
