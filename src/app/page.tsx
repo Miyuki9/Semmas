053d9a4277f02c473cd42ee0f210f2a1edb203d3
felix <felixmaundu1@gmail.com>
2024-09-17 10:06:03 +0300

    Render necessary page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import SignUp from "./signUp/page";
-import Financials from "./financials/page";
 
 
 export default function Home() {
   return (
     <div className="">
       <SignUp/>
-      <Financials/>
     </div>
   );
 }
